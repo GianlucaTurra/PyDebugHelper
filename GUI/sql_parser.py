d3a7099885c2241fbc7c9e610f2276b4e1ca6348
GianlucaTurra <gianlucaturra1@gmail.com>
2024-01-17 17:59:03 +0100

    Added little stuff to sql parser

--- GUI/sql_parser.py
+++ GUI/sql_parser.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import scrolledtext
 
+import sqlparse
+
 FRAMES = ['input', 'output']
 
 
@@ -16,6 +18,7 @@ class SqlParser(tk.Frame):
     def show_frame(self, frame_name: str) -> None:
         if frame_name == 'input':
             self.output_frame.grid_forget()
+            self.input_frame.input_box.delete('1.0', tk.END)
             self.input_frame.grid()
         if frame_name == 'output':
             self.input_frame.grid_forget()
@@ -31,17 +34,21 @@ class InputFrame(tk.Frame):
         self.title_label = tk.Label(self, text='Query sql da riformattare:', font=('Elvetica', 16))
         self.title_label.grid(row=0, column=0, padx=50, pady=25)
 
-        self.input_box = scrolledtext.ScrolledText(self, wrap=tk.WORD)
+        self.input_box = scrolledtext.ScrolledText(self, wrap=tk.WORD, width=177, height=35)
         self.input_box.grid(row=1, column=0, padx=50, pady=25)
 
         self.format_button = tk.Button(self,
                                        text='Formatta',
-                                       command=lambda: self.format_sql_string(self.input_box.get('1.0', 'end-1c')))
+                                       command=lambda: self.format_sql_string(
+                                           self.input_box.get('1.0', 'end-1c')
+                                       ),
+                                       font=('Elvetica', 14))
+        self.format_button.grid(row=2, column=0, padx=50, pady=15)
 
     def format_sql_string(self, sql_string: str) -> None:
-        self.parent.output_frame = OutputFrame(self.parent, sql_string)
+        formatted_sql_string = sqlparse.format(sql_string, reindent=True, keyword_case='upper', identifier_case='lower')
+        self.parent.output_frame = OutputFrame(self.parent, formatted_sql_string)
         self.parent.show_frame('output')
-        pass
 
 
 class OutputFrame(tk.Frame):
@@ -53,7 +60,13 @@ class OutputFrame(tk.Frame):
         self.title_label = tk.Label(self, text='Query sql da riformattare:', font=('Elvetica', 16))
         self.title_label.grid(row=0, column=0, padx=50, pady=25)
 
-        self.output_box = scrolledtext.ScrolledText(self, wrap=tk.WORD)
+        self.output_box = scrolledtext.ScrolledText(self, wrap=tk.WORD, width=177, height=35)
         self.output_box.insert(tk.END, formatted_string)
         self.output_box.configure(state='disabled')
         self.output_box.grid(row=1, column=0, padx=50, pady=25)
+
+        self.beck_to_insert = tk.Button(self,
+                                        text='Inserimento',
+                                        command=lambda: self.parent.show_frame('input'),
+                                        font=('Elvetica', 14))
+        self.beck_to_insert.grid(row=2, column=0, padx=50, pady=15)
--- application.py
+++ application.py
@@ -4,8 +4,9 @@ from GUI.app_notebook import AppNoteBook
 from GUI.insert_ds import InsertDs
 from GUI.sql_parser import SqlParser
 
-__root = tk.Tk()
 __frames = {'Controllo DS': InsertDs, 'Sql parser': SqlParser}
+__root = tk.Tk()
+__root.state('zoomed')
 __app = AppNoteBook(parent=__root, frames=__frames)
 __app.grid()
 
