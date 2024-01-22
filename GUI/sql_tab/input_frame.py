f22df4eae16c818aef7f5380c8e80d4a38486df1
GianlucaTurra <gianlucaturra1@gmail.com>
2024-01-22 17:25:02 +0100

    Fixed last things

--- GUI/sql_tab/input_frame.py
+++ GUI/sql_tab/input_frame.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 from tkinter import scrolledtext
 
-import sqlparse
-
 
 class InputFrame(tk.Frame):
 
@@ -23,10 +21,3 @@ class InputFrame(tk.Frame):
                                        ),
                                        font=('Elvetica', 14))
         self.format_button.grid(row=2, column=0, padx=50, pady=15)
-
-    def format_sql_string(self, sql_string: str) -> None:
-        formatted_sql_string = sqlparse.format(sql_string, reindent=True, keyword_case='upper', identifier_case='lower',
-                                               use_space_around_operators=True, output_format='rpgle')
-        self.input_box.delete('1.0', 'end-1c')
-        self.input_box.insert(tk.END, formatted_sql_string)
-        self.input_box.configure(state='disabled')
--- GUI/sql_tab/sql_parser.py
+++ GUI/sql_tab/sql_parser.py
@@ -1,10 +1,12 @@
 import tkinter as tk
+from tkinter import messagebox
 
 from GUI.sql_tab.input_frame import InputFrame
 
 import sqlparse
 
 from GUI.sql_tab.output_frame import OutputFrame
+from modules.ds_matcher import get_funny_exclamation
 
 FRAMES = ['input', 'output']
 
@@ -28,10 +30,11 @@ class SqlParser(tk.Frame):
             self.output_frame.grid()
 
     def format_sql_string(self, sql_string: str) -> None:
+        if len(sql_string) == 0:
+            text = f'{get_funny_exclamation()} qualcuno si è dimenticato di scrivere una query!'
+            messagebox.showerror(title='Errore di inserimento', message=text)
+            return
         formatted_sql_string = sqlparse.format(sql_string, reindent=True, keyword_case='upper', identifier_case='lower',
                                                use_space_around_operators=True, output_format='rpgle') # , wrap_after=35
         self.output_frame = OutputFrame(self, formatted_sql_string)
         self.show_frame('output')
-
-
-
